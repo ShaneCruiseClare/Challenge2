098eba54d155127847ba28b5fbe2aa4b6c8565b0
ShaneCruiseClare <B00147191@mytudublin.ie>
2022-10-15 16:35:44 +0100

    doc

---
+++ Challange2/Assets/Challenge 2/Scripts/Link.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://github.com/ShaneCruiseClare/Challenge2");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
